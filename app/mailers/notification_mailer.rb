d82ae14382c32e29f229753922847c698a09d041
brandongoodman615 <brandongoodman615@gmail.com>
2018-05-01 22:31:55 +0000

    couldn't get email to show up in inbox. moving on to try in production L41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "brandongoodman615@gmail.com",
+    mail(to: "brandontest1111@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
