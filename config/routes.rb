40de4e4426d67e9ac53298e67d62de9802d7b85a
brandongoodman615 <brandongoodman615@gmail.com>
2018-05-05 17:16:47 +0000

    made my profile button on navbar that links to user show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :comments, only: :create
     resources :photos, only: [ :create, :show ]
   end
+  resources :users, only: :show
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
